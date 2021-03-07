7b511e79c2e066f9ee310ee438bcb8766acbfa0c
Mick George <predatorie.mg@gmail.com>
2021-03-06 22:55:17 -0500

    List loading but not complete

--- lib/src/views/athletes_page.dart
+++ lib/src/views/athletes_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tcf_getit/src/models/athletes.dart';
 import 'package:tcf_getit/src/services/athletes_repository.dart';
 
 class AthletesPage extends StatefulWidget {
@@ -43,36 +44,40 @@ class _AthletesPageState extends State<AthletesPage> {
     final repository = context.watch<AthletesRepository>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ATHLETES'),
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                  child: Text('Get Next Athletes'),
-                  onPressed: () async {
-                    if (repository.hasNextPage) {
-                      await context
-                          .read<AthletesRepository>()
-                          .getNextAthletesAsync();
-
-                      if (repository.errorMessage.isNotEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text(repository.errorMessage)));
-                      }
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content:
-                              Text('EOL: ${repository.athletes.length * 22}')));
-                    }
-                  }),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('ATHLETES'),
         ),
+        body: ListView.builder(
+          shrinkWrap: true,
+          itemCount: repository.athletes.length,
+          itemBuilder: (_, index) {
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: repository.athletes[index].data.length,
+                itemBuilder: (_, i) {
+                  return _athleteCard(repository.athletes[index].data[i]);
+                });
+          },
+        ));
+  }
+
+  Widget _athleteCard(Datum data) {
+    final isEmpty = data.attributes.profileImageUrl == null ||
+        data.attributes.profileImageUrl.isEmpty;
+
+    return Center(
+      child: Card(
+        key: Key(data.id),
+        child: isEmpty
+            ? Image.asset(
+                'assets/images/tcf_logo_small.png',
+              )
+            : Image.network(
+                data.attributes.profileImageUrl,
+                fit: BoxFit.cover,
+                width: 256,
+                height: 256,
+              ),
       ),
     );
   }
